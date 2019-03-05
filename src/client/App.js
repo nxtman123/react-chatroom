564d9e39d1c4b12ed804bacfe9bccb7b92793caf
Kurtis Jantzen <kjjantze@ucalgary.ca>
2019-03-04 20:30:33 -0700

    Import material-ui and start the layout

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,43 @@
-import React, { Component } from 'react';
-import './app.css';
-import ReactImage from './react.png';
+import React, { Component, Fragment } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 
-export default class App extends Component {
-  state = { username: null };
+import Drawer from '@material-ui/core/Drawer';
+import Toolbar from '@material-ui/core/Toolbar';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import Icon from '@material-ui/core/Icon';
+import Typography from '@material-ui/core/Typography';
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+const styles = theme => ({
+    grow: { flexGrow: 1 }
+});
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
+class App extends Component {
+    state = { username: null };
+
+    componentDidMount() {
+        fetch('/api/getUsername')
+            .then(res => res.json())
+            .then(user => this.setState({ username: user.username }));
+    }
+
+    render(props) {
+        const { classes } = this.props;
+        const { username } = this.state;
+        return (
+            <Fragment>
+                <AppBar><Toolbar>
+                    <Typography variant="h6" color="inherit" className={classes.grow}>
+                        Chatroom
+                    </Typography>
+                    <IconButton color="inherit">
+                        <Icon>menu</Icon>
+                    </IconButton>
+                </Toolbar></AppBar>
+                {username ? `Hello ${username}` : <h1>Loading.. please wait!</h1>}
+            </Fragment>
+        );
+    }
 }
+
+export default withStyles(styles, { withTheme: true })(App);
